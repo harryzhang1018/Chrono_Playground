c97973e763cbbadc2bb491a7ce229aa45d220615
harry <hzhang699@wisc.edu>
2024-07-25 11:26:37 -0500

    add demos as back up

---
+++ truckHighway/demo_VEH_truck.py
@@ -0,0 +1,178 @@
+# =============================================================================
+# PROJECT CHRONO - http://projectchrono.org
+#
+# Copyright (c) 2014 projectchrono.org
+# All rights reserved.
+#
+# Use of this source code is governed by a BSD-style license that can be found
+# in the LICENSE file at the top level of the distribution and at
+# http://projectchrono.org/license-chrono.txt.
+#
+# =============================================================================
+
+import pychrono.core as chrono
+import pychrono.irrlicht as irr
+import pychrono.vehicle as veh
+import math
+
+"""
+!!!! Set this path before running the demo!
+"""
+chrono.SetChronoDataPath(chrono.GetChronoDataPath())
+veh.SetDataPath(chrono.GetChronoDataPath() + 'vehicle/')
+
+# Initial vehicle location and orientation
+initLoc = chrono.ChVector3d(7, -60, 0.5)
+initRot = chrono.QuatFromAngleZ(1.57)
+# initRot = chrono.ChQuaterniond(1, 0, 0, 0)
+
+# Visualization type for vehicle parts (PRIMITIVES, MESH, or NONE)
+chassis_vis_type = veh.VisualizationType_MESH
+suspension_vis_type = veh.VisualizationType_MESH
+steering_vis_type = veh.VisualizationType_MESH
+wheel_vis_type = veh.VisualizationType_MESH
+tire_vis_type = veh.VisualizationType_MESH  # : VisualizationType::PRIMITIVES
+# Collision type for chassis (PRIMITIVES, MESH, or NONE)
+chassis_collision_type = veh.CollisionType_NONE
+
+# Type of tire model (RIGID, TMEASY)
+tire_model = veh.TireModelType_RIGID
+
+# Rigid terrain
+# terrain_model = veh.RigidTerrain.BOX
+terrainHeight = 0      # terrain height
+terrainLength = 100.0  # size in X direction
+terrainWidth = 100.0   # size in Y direction
+
+# Poon chassis tracked by the camera
+trackPoint = chrono.ChVector3d(0.0, 0.0, 1.75)
+
+# Contact method
+contact_method = chrono.ChContactMethod_NSC
+contact_vis = False
+
+# Simulation step sizes
+step_size = 1e-3
+tire_step_size = step_size
+
+# Time interval between two render frames
+render_step_size = 1.0 / 50  # FPS = 50
+
+# =============================================================================
+
+# --------------
+# Create systems
+# --------------
+# Create the ARTcar vehicle, set parameters, and initialize
+car = veh.Kraz()
+car.SetContactMethod(contact_method)
+car.SetChassisCollisionType(chassis_collision_type)
+car.SetChassisFixed(False)
+car.SetInitPosition(chrono.ChCoordsysd(initLoc, initRot))
+car.Initialize()
+
+car.SetChassisVisualizationType(chassis_vis_type, chassis_vis_type)
+car.SetSteeringVisualizationType(steering_vis_type)
+car.SetSuspensionVisualizationType(suspension_vis_type, suspension_vis_type)
+car.SetWheelVisualizationType(wheel_vis_type, wheel_vis_type)
+car.SetTireVisualizationType(wheel_vis_type, wheel_vis_type)
+
+
+
+
+car.GetSystem().SetCollisionSystemType(chrono.ChCollisionSystem.Type_BULLET)
+
+# Create the terrain
+patch_mat = chrono.ChContactMaterialNSC()
+patch_mat.SetFriction(0.9)
+patch_mat.SetRestitution(0.01)
+terrain = veh.RigidTerrain(car.GetSystem())
+patch = terrain.AddPatch(patch_mat, 
+    chrono.ChCoordsysd(chrono.ChVector3d(0, 0, 0), chrono.QUNIT),
+    veh.GetDataFile('terrain/meshes/Highway_col.obj'),
+    True, 0.01, False)
+vis_mesh = chrono.ChTriangleMeshConnected().CreateFromWavefrontFile(veh.GetDataFile("terrain/meshes/Highway_vis.obj"), True, True)
+tri_mesh_shape = chrono.ChVisualShapeTriangleMesh()
+tri_mesh_shape.SetMesh(vis_mesh)
+tri_mesh_shape.SetMutable(False)
+patch.GetGroundBody().AddVisualShape(tri_mesh_shape)
+terrain.Initialize()
+
+
+# -------------------------------------
+# Create the vehicle Irrlicht interface
+# Create the driver system
+# -------------------------------------
+
+vis = veh.ChWheeledVehicleVisualSystemIrrlicht()
+vis.SetWindowTitle('truck vehicle simulation')
+vis.SetWindowSize(1280, 1024)
+vis.SetChaseCamera(trackPoint, 25.0, 1.5)
+vis.Initialize()
+vis.AddLogo(chrono.GetChronoDataFile('logo_pychrono_alpha.png'))
+vis.AddLightDirectional()
+vis.AddSkyBox()
+vis.AttachVehicle(car.GetTractor())
+
+
+# Create the driver system
+driver = veh.ChInteractiveDriverIRR(vis)
+
+# Set the time response for steering and throttle keyboard inputs.
+steering_time = 1.0  # time to go from 0 to +1 (or from 0 to -1)
+throttle_time = 1.0  # time to go from 0 to +1
+braking_time = 0.3   # time to go from 0 to +1
+driver.SetSteeringDelta(render_step_size / steering_time)
+driver.SetThrottleDelta(render_step_size / throttle_time)
+driver.SetBrakingDelta(render_step_size / braking_time)
+
+driver.Initialize()
+
+# ---------------
+# Simulation loop
+# ---------------
+
+# output vehicle mass
+print( "VEHICLE MASS: ",  car.GetTractor().GetMass())
+
+# Number of simulation steps between miscellaneous events
+render_steps = math.ceil(render_step_size / step_size)
+
+# Initialize simulation frame counter s
+realtime_timer = chrono.ChRealtimeStepTimer()
+step_number = 0
+render_frame = 0
+
+while vis.Run() :
+    time = car.GetSystem().GetChTime()
+    # get trailer and tractor position
+    tractor_pos = car.GetTractorChassisBody().GetPos()
+    trailer_pos = car.GetTrailer().GetChassis().GetBody().GetPos()
+    # Render scene and output POV-Ray data
+    if (step_number % render_steps == 0) :
+        vis.BeginScene()
+        vis.Render()
+        vis.EndScene()
+        render_frame += 1
+
+    # Get driver inputs
+    driver_inputs = driver.GetInputs()
+
+    # Update modules (process inputs from other modules)
+    driver.Synchronize(time)
+    terrain.Synchronize(time)
+    car.Synchronize(time, driver_inputs, terrain)
+    vis.Synchronize(time, driver_inputs)
+
+    # Advance simulation for one timestep for all modules
+    driver.Advance(step_size)
+    terrain.Advance(step_size)
+    car.Advance(step_size)
+    vis.Advance(step_size)
+
+    # Increment frame number
+    step_number += 1
+
+    # Spin in place for real time to catch up
+    realtime_timer.Spin(step_size)
+

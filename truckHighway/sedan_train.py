6c9aac153100af682d5cb4cd561938a791b70560
harry <hzhang699@wisc.edu>
2024-09-20 23:53:49 -0500

    push new demo for sedan and more complexed trajectory

---
+++ truckHighway/sedan_train.py
@@ -0,0 +1,460 @@
+# =============================================================================
+# PROJECT CHRONO - http://projectchrono.org
+#
+# Copyright (c) 2014 projectchrono.org
+# All rights reserved.
+#
+# Use of this source code is governed by a BSD-style license that can be found
+# in the LICENSE file at the top level of the distribution and at
+# http://projectchrono.org/license-chrono.txt.
+#
+# =============================================================================
+
+import pychrono.core as chrono
+import pychrono.irrlicht as irr
+import pychrono.vehicle as veh
+import math
+import numpy as np
+import time
+from controller import error_state
+from merge_scenario import merging_scenario,parallel_scenario,serial_scenario
+import csv
+"""
+!!!! Set this path before running the demo!
+"""
+chrono.SetChronoDataPath(chrono.GetChronoDataPath())
+veh.SetDataPath(chrono.GetChronoDataPath() + 'vehicle/')
+
+# Initial vehicle location and orientation
+initLoc = chrono.ChVector3d(-500, 0, 0.3)
+initRot = chrono.QuatFromAngleZ(0)
+
+# initRot = chrono.ChQuaterniond(1, 0, 0, 0)
+
+# Visualization type for vehicle parts (PRIMITIVES, MESH, or NONE)
+# Visualization type for vehicle parts (PRIMITIVES, MESH, or NONE)
+vis_type = veh.VisualizationType_MESH
+# Collision type for chassis (PRIMITIVES, MESH, or NONE)
+chassis_collision_type = veh.CollisionType_NONE
+
+# Type of tire model (RIGID, TMEASY)
+tire_model = veh.TireModelType_TMEASY
+
+# Rigid terrain
+# terrain_model = veh.RigidTerrain.BOX
+terrainHeight = 0      # terrain height
+terrainLength = 1200.0  # size in X direction
+terrainWidth = 1200.0   # size in Y direction
+
+# Poon chassis tracked by the camera
+trackPoint = chrono.ChVector3d(-5.0, 0.0, 1.8)
+
+# Contact method
+contact_method = chrono.ChContactMethod_NSC
+contact_vis = False
+
+# Simulation step sizes
+step_size = 1e-3
+tire_step_size = step_size
+
+# Time interval between two render frames
+render_step_size = 1.0 / 25  # FPS = 50
+control_step_size = 1.0 / 20 # FPS to run control = 20
+
+# =============================================================================
+
+# --------------
+# Create systems
+# --------------
+# Create the sedan vehicle, set parameters, and initialize
+sedan = veh.BMW_E90()
+sedan.SetContactMethod(contact_method)
+sedan.SetChassisCollisionType(chassis_collision_type)
+sedan.SetChassisFixed(False)
+sedan.SetInitPosition(chrono.ChCoordsysd(initLoc, initRot))
+sedan.SetTireType(tire_model)
+sedan.SetTireStepSize(tire_step_size)
+
+
+sedan.Initialize()
+
+sedan.SetChassisVisualizationType(vis_type)
+sedan.SetSuspensionVisualizationType(vis_type)
+sedan.SetSteeringVisualizationType(vis_type)
+sedan.SetWheelVisualizationType(vis_type)
+sedan.SetTireVisualizationType(vis_type)
+
+
+
+#sedan.GetSystem().SetCollisionSystemType(chrono.ChCollisionSystem.Type_BULLET)
+
+# Create the terrain
+
+patch_mat = chrono.ChContactMaterialNSC()
+patch_mat.SetFriction(0.9)
+patch_mat.SetRestitution(0.01)
+terrain = veh.RigidTerrain(sedan.GetSystem())
+patch = terrain.AddPatch(patch_mat, 
+    chrono.ChCoordsysd(chrono.ChVector3d(0, 0, 0), chrono.QUNIT), 
+    terrainLength, terrainWidth)
+
+patch.SetTexture(veh.GetDataFile("terrain/textures/Concrete002_2K-JPG/Concrete002_2K_Color.jpg"), 100, 100)
+patch.SetColor(chrono.ChColor(0.8, 0.8, 0.5))
+terrain.Initialize()
+
+# adding visualization for the reference trajectory
+# Create visualization material for the path
+vis_mat_path = chrono.ChVisualMaterial()
+vis_mat_path.SetDiffuseColor(chrono.ChColor(0.0, 1.0, 0.0))
+road_vis_mat = chrono.ChVisualMaterial()
+road_vis_mat.SetDiffuseColor(chrono.ChColor(1.0, 1.0, 1.0))
+# Create ChBodyEasyBox objects at specified positions
+# reference centerline of the sedan
+# reference_trajectory = np.loadtxt('./data/reference_traj/trajectory.csv', delimiter=',')
+reference_trajectory = np.loadtxt('./data/reference_traj/trajectory_complex.csv', delimiter=',')
+# only show sparse points not all points
+visual_reference_trajectory = reference_trajectory[::50]
+for pos in visual_reference_trajectory:
+    center_x,center_y,center_heading = pos
+    box_body = chrono.ChBodyEasyCylinder(chrono.ChAxis_Z, 0.2, 0.05, 1000, True, False)
+    box_body.SetPos(chrono.ChVector3d(center_x, center_y, 0.5))
+    box_body.SetFixed(True)
+    # Set visual material for the box
+    shape = box_body.GetVisualModel().GetShape(0)
+    if shape.GetNumMaterials() == 0:
+        shape.AddMaterial(vis_mat_path)
+    else:
+        shape.GetMaterials()[0] = vis_mat_path
+        
+        
+    right_lane_x = center_x + 1.85 * np.cos(center_heading+np.pi/2)
+    right_lane_y = center_y + 1.85 * np.sin(center_heading+np.pi/2)
+    left_lane_x = center_x + 1.85 * np.cos(center_heading-np.pi/2)
+    left_lane_y = center_y + 1.85 * np.sin(center_heading-np.pi/2)
+    lane_rot = chrono.QuatFromAngleZ(center_heading)
+    # add right lane and left lane
+    right_lane_body = chrono.ChBodyEasyBox(1.5, 0.1, 0.05, 1000, True, False)
+    right_lane_body.SetPos(chrono.ChVector3d(right_lane_x, right_lane_y, 0.5))
+    right_lane_body.SetRot(lane_rot)
+    right_lane_body.SetFixed(True)
+    right_lane_shape = right_lane_body.GetVisualModel().GetShape(0)
+    right_lane_shape.AddMaterial(road_vis_mat)
+    left_lane_body = chrono.ChBodyEasyBox(1.5, 0.1, 0.05, 1000, True, False)
+    left_lane_body.SetPos(chrono.ChVector3d(left_lane_x, left_lane_y, 0.5))
+    left_lane_body.SetRot(lane_rot)
+    left_lane_body.SetFixed(True)
+    left_lane_shape = left_lane_body.GetVisualModel().GetShape(0)
+    left_lane_shape.AddMaterial(road_vis_mat)
+    sedan.GetSystem().Add(right_lane_body)
+    sedan.GetSystem().Add(left_lane_body)
+    
+    #sedan.GetSystem().Add(box_body)
+
+# initialize the ball to represent the ball from right hand merging
+ball_merge_right = chrono.ChBodyEasySphere(0.4, 1000, True, False)
+vis_merge = chrono.ChVisualMaterial()
+vis_merge.SetDiffuseColor(chrono.ChColor(1,0,0))
+ball_merge_right_shape = ball_merge_right.GetVisualModel().GetShape(0)
+ball_merge_right_shape.AddMaterial(vis_merge)
+ball_merge_right.SetPos(chrono.ChVector3d(0,0,0))
+ball_merge_right.SetFixed(False)
+sedan.GetSystem().Add(ball_merge_right)
+# initialize the ball to represent the ball from left hand merging
+ball_merge_left = chrono.ChBodyEasySphere(0.4, 1000, True, False)
+vis_merge = chrono.ChVisualMaterial()
+vis_merge.SetDiffuseColor(chrono.ChColor(1,0,0))
+ball_merge_left_shape = ball_merge_left.GetVisualModel().GetShape(0)
+ball_merge_left_shape.AddMaterial(vis_merge)
+ball_merge_left.SetPos(chrono.ChVector3d(0,0,0))
+ball_merge_left.SetFixed(False)
+sedan.GetSystem().Add(ball_merge_left)
+# initialize the ball to represent the ball from right hand side parallel case
+ball_paral_right = chrono.ChBodyEasySphere(0.4, 1000, True, False)
+vis_paral = chrono.ChVisualMaterial()
+vis_paral.SetDiffuseColor(chrono.ChColor(0,0,1))
+ball_paral_right_shape = ball_paral_right.GetVisualModel().GetShape(0)
+ball_paral_right_shape.AddMaterial(vis_paral)
+ball_paral_right.SetPos(chrono.ChVector3d(0,0,0))
+ball_paral_right.SetFixed(False)
+sedan.GetSystem().Add(ball_paral_right)
+# initialize the ball to represent the ball from left hand side parallel case
+ball_paral_left = chrono.ChBodyEasySphere(0.4, 1000, True, False)
+vis_paral = chrono.ChVisualMaterial()
+vis_paral.SetDiffuseColor(chrono.ChColor(0,0,1))
+ball_paral_left_shape = ball_paral_left.GetVisualModel().GetShape(0)
+ball_paral_left_shape.AddMaterial(vis_paral)
+ball_paral_left.SetPos(chrono.ChVector3d(0,0,0))
+ball_paral_left.SetFixed(False)
+sedan.GetSystem().Add(ball_paral_left)
+# initialize the ball to represent the ball from serial case
+ball_serial = chrono.ChBodyEasySphere(0.4, 1000, True, False)
+vis_serial = chrono.ChVisualMaterial()
+vis_serial.SetDiffuseColor(chrono.ChColor(0,1,0))
+ball_serial_shape = ball_serial.GetVisualModel().GetShape(0)
+ball_serial_shape.AddMaterial(vis_serial)
+ball_serial.SetPos(chrono.ChVector3d(0,0,0))
+ball_serial.SetFixed(False)
+sedan.GetSystem().Add(ball_serial)
+# -------------------------------------
+# Create the vehicle Irrlicht interface
+# Create the driver system
+# -------------------------------------
+
+vis = veh.ChWheeledVehicleVisualSystemIrrlicht()
+vis.SetWindowTitle('sedan vehicle simulation')
+vis.SetWindowSize(1280, 1024)
+vis.SetChaseCamera(trackPoint, 6.0, 0.5)
+vis.Initialize()
+vis.AddLogo(chrono.GetChronoDataFile('logo_pychrono_alpha.png'))
+vis.AddLightDirectional()
+# vis.AddTypicalLights()
+vis.AddSkyBox()
+vis.AttachVehicle(sedan.GetVehicle())
+
+
+# Create the driver system
+driver = veh.ChInteractiveDriverIRR(vis)
+# Set the time response for steering and throttle keyboard inputs.
+steering_time = 1.0  # time to go from 0 to +1 (or from 0 to -1)
+throttle_time = 1.0  # time to go from 0 to +1
+braking_time = 0.3   # time to go from 0 to +1
+driver.SetSteeringDelta(render_step_size / steering_time)
+driver.SetThrottleDelta(render_step_size / throttle_time)
+driver.SetBrakingDelta(render_step_size / braking_time)
+
+driver.Initialize()
+# ---------------
+# Simulation loop
+# ---------------
+
+# Number of simulation steps between miscellaneous events
+render_steps = math.ceil(render_step_size / step_size)
+control_steps = math.ceil(control_step_size / step_size)
+
+# Initialize simulation frame counter s
+realtime_timer = chrono.ChRealtimeStepTimer()
+step_number = 0
+control_number = 0
+render_frame = 0
+is_merging = False
+is_merging_left = False
+merge_trajectory = []
+merge_trajectory_left = []
+merge_traj_ind = 0
+merge_traj_ind_left = 0
+
+is_parallel = False
+is_parallel_left = False
+parallel_trajectory = []
+parallel_trajectory_left = []
+parallel_traj_ind = 0
+parallel_traj_ind_left = 0
+
+is_serial = False
+serial_trajectory = []
+serial_traj_ind = 0
+
+# Constants
+merge_start_time = 5.0
+min_pause = 1.0  # Minimum pause between trajectories
+max_pause = 2.0  # Maximum pause between trajectories
+
+# Variables to track last trajectory generation time
+last_trajectory_time = 0
+next_trajectory_time = merge_start_time
+
+# Variables to track the last side used for each scenario type
+last_merge_side = None
+last_parallel_side = None
+
+while vis.Run() :
+    time = sedan.GetSystem().GetChTime()
+    # control sedan vehicle 
+    # driver_sedan = straight() # lanechange
+    # get trailer and tractor position
+    veh_pos = sedan.GetChassisBody().GetPos()
+    veh_heading = sedan.GetChassisBody().GetRot().GetCardanAnglesZYX().z
+    veh_x = veh_pos.x
+    veh_y = veh_pos.y
+    veh_heading = veh_heading
+    veh_back_x = veh_x - 5.74 * np.cos(veh_heading)
+    veh_back_y = veh_y - 5.74 * np.sin(veh_heading)
+    
+
+    if time > next_trajectory_time:
+
+        # Randomly choose between tractor and trailer position
+        if np.random.rand() < 0.5:
+            sedan_x, sedan_y = veh_x, veh_y
+            position_used = "parallel"
+        else:
+            sedan_x, sedan_y = veh_back_x, veh_back_y
+            position_used = "behind"
+
+        # Randomly choose between merge, parallel, and serial scenarios
+        scenario_choice = np.random.rand()
+        
+        if scenario_choice < 0.4:  # 40% chance for merge
+            # Merge scenario
+            from_side = 'left' if last_merge_side != 'left' else 'right'
+            vel = np.random.uniform(26, 30)
+            merge_time = np.random.uniform(2.5, 4.5)
+            
+            new_trajectory = merging_scenario(reference_trajectory, sedan_x, sedan_y, 
+                                              from_left_right=from_side,
+                                              vel=vel,
+                                              merge_time=merge_time,
+                                              freq=25)
+            print(f"Merging from {from_side}, velocity: {vel:.2f}, merge time: {merge_time:.2f}, using {position_used} position")
+            print(new_trajectory.shape)
+            
+            if from_side == 'left':
+                merge_trajectory_left = new_trajectory
+                is_merging_left = True
+                merge_traj_ind_left = 0
+            else:
+                merge_trajectory = new_trajectory
+                is_merging = True
+                merge_traj_ind = 0
+            
+            last_merge_side = from_side
+        elif scenario_choice < 0.8:  # 40% chance for parallel
+            # Parallel scenario
+            from_side = 'left' if last_parallel_side != 'left' else 'right'
+            driver_vel = np.random.uniform(30, 40)
+            run_time = np.random.uniform(3.5, 5.5)
+            
+            new_trajectory = parallel_scenario(reference_trajectory, sedan_x, sedan_y, 
+                                               from_left_right=from_side,
+                                               vel=driver_vel,
+                                               run_time=run_time,
+                                               freq=25)
+            print(f"Parallel from {from_side}, velocity: {driver_vel:.2f}, run time: {run_time:.2f}, using {position_used} position")
+            print(new_trajectory.shape)
+            
+            if from_side == 'left':
+                parallel_trajectory_left = new_trajectory
+                is_parallel_left = True
+                parallel_traj_ind_left = 0
+            else:
+                parallel_trajectory = new_trajectory
+                is_parallel = True
+                parallel_traj_ind = 0
+            
+            last_parallel_side = from_side
+        else:  # 20% chance for serial, but only if no merging is active
+            if not is_merging and not is_merging_left:
+                # Serial scenario
+                driver_vel = np.random.uniform(26, 40)
+                run_time = np.random.uniform(3.5, 5.5)
+                
+                serial_trajectory = serial_scenario(reference_trajectory, sedan_x, sedan_y, 
+                                                    vel=driver_vel,
+                                                    run_time=run_time,
+                                                    freq=25)
+                print(f"Serial scenario, velocity: {driver_vel:.2f}, run time: {run_time:.2f}, using {position_used} position")
+                print(serial_trajectory.shape)
+                
+                is_serial = True
+                serial_traj_ind = 0
+            else:
+                print("Serial scenario skipped due to active merging")
+
+        # Set the next trajectory generation time
+        last_trajectory_time = time
+        next_trajectory_time = time + np.random.uniform(min_pause, max_pause)
+
+    if (step_number % control_steps == 0):
+        # for sedan controller now it's a simple PID controller
+        error = error_state(veh_state=[veh_x,veh_y,veh_heading],ref_traj=reference_trajectory,lookahead=3.0)
+        steering = sum([x * y for x, y in zip(error, [0.02176878 , 0.72672704 , 0.78409284 ,-0.0105355 ])]) # @hang here is the place to add controller
+        ref_vel = 30
+        current_vel = sedan.GetVehicle().GetSpeed()
+        vel_error = ref_vel - current_vel
+        throttle = 0.5 * vel_error + 0.5
+        driver.SetSteering(steering)
+        driver.SetThrottle(throttle)
+    
+    # Render scene and output POV-Ray data
+    if (step_number % render_steps == 0) :
+        vis.BeginScene()
+        # print(tractor_pos)
+        # print(tractor_heading)
+        # Right merge
+        if is_merging and merge_traj_ind < len(merge_trajectory):
+            ball_merge_right.SetPos(chrono.ChVector3d(merge_trajectory[merge_traj_ind][0], merge_trajectory[merge_traj_ind][1], 0.5))
+            merge_traj_ind += 1
+        elif is_merging:
+            is_merging = False
+            merge_traj_ind = 0
+            
+        vir_veh_1 = [ball_merge_right.GetPos().x, ball_merge_right.GetPos().y] if is_merging else [0,0]
+
+        # Left merge
+        if is_merging_left and merge_traj_ind_left < len(merge_trajectory_left):
+            ball_merge_left.SetPos(chrono.ChVector3d(merge_trajectory_left[merge_traj_ind_left][0], merge_trajectory_left[merge_traj_ind_left][1], 0.5))
+            merge_traj_ind_left += 1
+        elif is_merging_left:
+            is_merging_left = False
+            merge_traj_ind_left = 0
+            
+        vir_veh_2 = [ball_merge_left.GetPos().x, ball_merge_left.GetPos().y] if is_merging_left else [0,0]
+        # Right parallel
+        if is_parallel and parallel_traj_ind < len(parallel_trajectory):
+            ball_paral_right.SetPos(chrono.ChVector3d(parallel_trajectory[parallel_traj_ind][0], parallel_trajectory[parallel_traj_ind][1], 0.5))
+            parallel_traj_ind += 1
+        elif is_parallel:
+            is_parallel = False
+            parallel_traj_ind = 0
+        
+        vir_veh_3 = [ball_paral_right.GetPos().x, ball_paral_right.GetPos().y] if is_parallel else [0,0]
+
+        # Left parallel
+        if is_parallel_left and parallel_traj_ind_left < len(parallel_trajectory_left):
+            ball_paral_left.SetPos(chrono.ChVector3d(parallel_trajectory_left[parallel_traj_ind_left][0], parallel_trajectory_left[parallel_traj_ind_left][1], 0.5))
+            parallel_traj_ind_left += 1
+        elif is_parallel_left:
+            is_parallel_left = False
+            parallel_traj_ind_left = 0
+
+        vir_veh_4 = [ball_paral_left.GetPos().x, ball_paral_left.GetPos().y] if is_parallel_left else [0,0]
+        # Serial
+        if is_serial and serial_traj_ind < len(serial_trajectory):
+            ball_serial.SetPos(chrono.ChVector3d(serial_trajectory[serial_traj_ind][0], serial_trajectory[serial_traj_ind][1], 0.5))
+            serial_traj_ind += 1
+        elif is_serial:
+            is_serial = False
+            serial_traj_ind = 0
+            
+        vir_veh_5 = [ball_serial.GetPos().x, ball_serial.GetPos().y] if is_serial else [0,0]
+        # write virtual vehicles position and sedan state into csv file
+        with open('./data/training_data/sedan_train_data.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow([time, veh_x, veh_y, veh_heading, vir_veh_1[0], vir_veh_1[1], vir_veh_2[0], vir_veh_2[1], vir_veh_3[0], vir_veh_3[1], vir_veh_4[0], vir_veh_4[1], vir_veh_5[0], vir_veh_5[1]])
+        vis.Render()
+        vis.EndScene()
+        # filename = './prototype/img_' + str(render_frame) +'.jpg' 
+        # vis.WriteImageToFile(filename)
+        render_frame += 1
+        
+
+    # Get driver inputs
+    driver_inputs = driver.GetInputs()
+    # Update modules (process inputs from other modules)
+    driver.Synchronize(time)
+    terrain.Synchronize(time)
+    sedan.Synchronize(time, driver_inputs, terrain)
+    vis.Synchronize(time, driver_inputs)
+
+    # Advance simulation for one timestep for all modules
+    driver.Advance(step_size)
+    terrain.Advance(step_size)
+    sedan.Advance(step_size)
+    vis.Advance(step_size)
+
+    # Increment frame number
+    step_number += 1
+
+    # Spin in place for real time to catch up
+    realtime_timer.Spin(step_size)
+
--- truckHighway/traj_gen.py
+++ truckHighway/traj_gen.py
@@ -22,30 +22,60 @@ def generate_ellipse_trajectory(a, b, num_points, center_x=0, center_y=0):
     
     return list(zip(x, y, heading))
 
+def generate_synthetic_trajectory(length_of_trajectory):
+    # define key points of the trajectory
+    keypoints = np.array([
+        [-500, 0],
+        [-400, 0],
+        [-300, 0],
+        [-100, -200],
+        [100, -400],
+        [300, -400],
+        [500, -200],
+        [500, 0],
+        [0, 400],
+        [-500, 0]
+    ])
+    
+    # Calculate cumulative distances between keypoints
+    distances = np.sqrt(np.sum(np.diff(keypoints, axis=0)**2, axis=1))
+    cumulative_distances = np.concatenate(([0], np.cumsum(distances)))
+    from scipy.interpolate import CubicSpline
+    # Create cubic spline functions for x and y
+    cs_x = CubicSpline(cumulative_distances, keypoints[:, 0])
+    cs_y = CubicSpline(cumulative_distances, keypoints[:, 1])
+    
+    # Generate equally spaced points along the trajectory
+    t = np.linspace(0, cumulative_distances[-1], length_of_trajectory)
+    x = cs_x(t)
+    y = cs_y(t)
+    dx = np.diff(x, append=x[0])
+    dy = np.diff(y, append=y[0])
+    heading = np.arctan2(dy, dx )
+    # Combine x and y into a single array of points
+    trajectory = np.vstack((x, y, heading)).T
+    return trajectory, keypoints
 # Example usage
 if __name__ == "__main__":
-    a, b = 500, 300  # Semi-major and semi-minor axes
-    num_points = 10000
-    trajectory = generate_ellipse_trajectory(a, b, num_points)
+    trajectory, keypoints = generate_synthetic_trajectory(10000)
     
     # # Print the first few points of the trajectory
     # for i, (x, y, heading) in enumerate(trajectory[:5]):
     #         print(f"Point {i}: x={x:.2f}, y={y:.2f}, heading={np.degrees(heading):.2f} degrees")
     # save the trajectory to a file
-    np.savetxt('trajectory.csv', trajectory, delimiter=',',fmt='%f')
+    np.savetxt('trajectory_complex.csv', trajectory, delimiter=',',fmt='%f')
     # Visualize the trajectory
     import matplotlib.pyplot as plt
 
-    # Extract x and y coordinates from the trajectory
-    x_coords, y_coords, _ = zip(*trajectory)
-
-    # Create a new figure and plot the trajectory
     plt.figure(figsize=(10, 8))
-    plt.plot(x_coords, y_coords, 'b-')
-    plt.scatter(x_coords, y_coords, c='r', s=20)
-    plt.title('Ellipse Trajectory')
+    plt.plot(trajectory[:, 0], trajectory[:, 1], label='Trajectory')
+    plt.plot(trajectory[0, 0], trajectory[0, 1], 'go', label='Start Point')
+    plt.plot(trajectory[-1, 0], trajectory[-1, 1], 'ro', label='End Point')
+    plt.plot(keypoints[:, 0], keypoints[:, 1], 'kx', label='Keypoints')
+    plt.legend()
+    plt.title('Synthetic Trajectory through Keypoints')
     plt.xlabel('X')
     plt.ylabel('Y')
-    plt.axis('equal')
     plt.grid(True)
+    plt.axis('equal')
     plt.show()
